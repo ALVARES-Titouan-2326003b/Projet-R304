94bfc119584657a0528664ce1b9513a8c2432196
Titouan <titouan.alvares@etu-univ-amu.fr>
2024-11-14 17:23:32 +0100

    feat: interface mort-vivant

--- src/main/java/com/hopitalfantastique/creatures/comportements/MortVivant.java
+++ src/main/java/com/hopitalfantastique/creatures/comportements/MortVivant.java
@@ -1,4 +1,12 @@
 package java.com.hopitalfantastique.creatures.comportements;
 
+import java.com.hopitalfantastique.creatures.Creature;
+import java.com.hopitalfantastique.creatures.maladies.Maladie;
+import java.util.ArrayList;
+
 public interface MortVivant {
+    public default void regenere(Creature creature) {
+        creature.setListeMaladie(new ArrayList<Maladie>());
+        creature.setIndMoral(0);
+    }
 }
