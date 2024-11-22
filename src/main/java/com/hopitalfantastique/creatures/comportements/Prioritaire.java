6092a028208383ce15c56b3405df61c70ab56267
Titouan <titouan.alvares@etu-univ-amu.fr>
2024-11-22 14:11:24 +0100

    chore

--- src/main/java/com/hopitalfantastique/creatures/comportements/Prioritaire.java
+++ src/main/java/com/hopitalfantastique/creatures/comportements/Prioritaire.java
@@ -1,4 +1,5 @@
 package java.com.hopitalfantastique.creatures.comportements;
 
 public interface Prioritaire {
+    // diminue le moral de plus en plus vite au fur et à mesure qu'il attend
 }
--- src/main/java/com/hopitalfantastique/creatures/comportements/Triage.java
+++ src/main/java/com/hopitalfantastique/creatures/comportements/Triage.java
@@ -1,4 +1,5 @@
 package java.com.hopitalfantastique.creatures.comportements;
 
 public interface Triage {
+    // attend plus patiemment quand il attend avec une créature de son espèce
 }
