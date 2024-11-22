133ad299f34f22541ea4f427a5b8c5c843c2321b
Titouan <titouan.alvares@etu-univ-amu.fr>
2024-11-22 16:24:19 +0100

    chore: nom méthode regenere -> meurt

--- src/main/java/com/hopitalfantastique/creatures/comportements/MortVivant.java
+++ src/main/java/com/hopitalfantastique/creatures/comportements/MortVivant.java
@@ -5,7 +5,7 @@ import java.com.hopitalfantastique.creatures.maladies.Maladie;
 import java.util.ArrayList;
 
 public interface MortVivant {
-    public default void regenere(CreaturePatient creature) {
+    public default void meurt(CreaturePatient creature) {
         creature.setListeMaladie(new ArrayList<Maladie>());
         creature.setIndMoral(0);
     }
