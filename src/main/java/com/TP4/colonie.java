f4385f91479238ea94515f2643740e251f94e3d2
PELLET-Casimir-2326084 <casimir.pellet@etu.univ-amu.fr>
2024-11-28 16:32:32 +0100

    creation de la class colonie

---
+++ src/main/java/com/TP4/colonie.java
@@ -0,0 +1,71 @@
+package java.com.TP4;
+
+import java.util.ArrayList;
+
+public class colonie {
+    private ArrayList<Meute> listMeute;
+    private String saison;
+
+    public void setSaison(String saison) {
+        this.saison = saison;
+    }
+
+    public colonie(ArrayList<Meute> listMeute, String saison) {
+        this.listMeute = listMeute;
+        this.saison = saison;
+    }
+
+    public ArrayList<Meute> getMeute() {
+        return listMeute;
+    }
+
+    public void ajouteMeute(Meute meute){
+        listMeute.add(meute);
+    }
+
+    public void supprMeute(Meute meute){
+        listMeute.remove(meute);
+    }
+
+    public String toString(){
+        String s = "Colonie{";
+        for(Meute meute: listMeute){
+            s += meute.toString() + ";";
+        }
+        s +="}";
+        return s;
+    }
+
+    public void passeSaison(){
+        switch (saison){
+            case "ete":
+                setSaison("automne");
+                for (Meute meute: listMeute){
+                    meute.hurlement();
+                }
+                break;
+            case "automne":
+                setSaison("hiver");
+                for (Meute meute: listMeute){
+                    meute.faitVeillir();
+                }
+                break;
+            case "hiver":
+                for (Meute meute: listMeute){
+                    meute.evolutionHiérarchie;
+                    meute.faitHumain();
+                }
+                setSaison("printemps");
+                break;
+            case "printemps":
+                for (Meute meute: listMeute){
+                    meute.reproduction();
+                }
+                setSaison("ete");
+                break;
+            default:
+                setSaison("ete");
+                this.passeSaison();
+        }
+    }
+}
