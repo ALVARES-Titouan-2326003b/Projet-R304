1d1e6543e4b6165262abfb20d6bbfc08da858bcf
p23000865 <casimir.pellet@etu.univ-amu.fr>
2024-12-02 17:56:47 +0100

    Ajout de tout les commentaires

--- src/main/java/com/TP4/Lycanthrope.java
+++ src/main/java/com/TP4/Lycanthrope.java
@@ -11,6 +11,16 @@ public class Lycanthrope {
     private int impetuosite;
     private Meute meute;
 
+    /**
+     * Constructeur de la classe Lycanthrope.
+     * Initialise un Lycanthrope avec les informations de nom, sexe, catégorie d'âge, force et meute.
+     *
+     * @param nom Le nom du Lycanthrope.
+     * @param sexe Le sexe du Lycanthrope.
+     * @param catAge La catégorie d'âge du Lycanthrope.
+     * @param force La force du Lycanthrope.
+     * @param meute La meute à laquelle appartient le Lycanthrope.
+     */
     public Lycanthrope(String nom, String sexe, String catAge, int force, Meute meute) {
         this.nom = nom;
         this.sexe = sexe;
@@ -23,38 +33,84 @@ public class Lycanthrope {
         this.meute = meute;
     }
 
+    // Getters et setters pour chaque attribut
+
+    /**
+     * Calcule le niveau de domination du Lycanthrope.
+     * Cette méthode devra être affinée en fonction des critères de domination.
+     *
+     * @return Le niveau de domination.
+     */
+    public int calculDomination() {
+        return 0; // a reflechir
+    }
+
+    /**
+     * Calcule le niveau d'un Lycanthrope en fonction de son âge, de son rang et de sa force.
+     * Le niveau augmente en fonction de la catégorie d'âge et du rang du Lycanthrope.
+     *
+     * @return Le niveau total du Lycanthrope.
+     */
+    public int calculNiveau() {
+        niveau = 0;
+        if (this.catAge == "jeune") {
+            niveau += 1;
+        } else if (this.catAge == "adulte") {
+            niveau += 10;
+        } else if (this.catAge == "vieux") {
+            niveau += 5;
+        }
+        niveau += 24 - "αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(this.getRang());
+        return niveau + this.facteurDomination + force;
+    }
+
+    /**
+     * Vieillit le Lycanthrope. Change sa catégorie d'âge ou le retire de la meute si nécessaire.
+     */
+    public void veillir() {
+        switch (this.catAge) {
+            case "jeune":
+                this.setCatAge("adulte");
+                break;
+            case "adulte":
+                this.setCatAge("vieux");
+                break;
+            case "vieux":
+                this.getMeute().enleverLycanthrope(this);  // Retire le Lycanthrope de la meute
+                break;
+        }
+    }
+
+    /**
+     * Effectue une tentative de domination entre deux Lycanthropes.
+     * Le Lycanthrope actuel tente de dominer un autre Lycanthrope en fonction de leur force et rang.
+     *
+     * @param lycanthrope Le Lycanthrope que l'on tente de dominer.
+     */
+    public void domination(Lycanthrope lycanthrope) {
+        if (this.getForce() * this.impetuosite >= lycanthrope.getForce() && !(lycanthrope.getSexe() == "femelle" && lycanthrope.getRang() == "α")) {
+            if (this.getForce() > lycanthrope.getForce() || lycanthrope.getRang() == "ω") {
+                if ("αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(this.rang) <= "αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(lycanthrope.getRang())) {
+                    Meute tmp = this.meute;
+                    this.setMeute(lycanthrope.getMeute());
+                    lycanthrope.setMeute(tmp);
+                    this.calculDomination();
+                    lycanthrope.calculDomination();
+                }
+            } else {
+                this.setFacteurDomination(this.getFacteurDomination() + 1);
+                lycanthrope.setFacteurDomination(lycanthrope.getFacteurDomination() - 1);
+            }
+        }
+    }
+
+
     public String getSexe() {
         return sexe;
     }
 
     public void setSexe(String sexe) {
         this.sexe = sexe;
-    }
-
-    public int calculDomination(){
-        return 0; // a reflechir
-    }
-
-    public int calculNiveau(){
-        niveau = 0;
-        if (this.catAge == "jeune"){
-            niveau += 1;
-        } else if (this.catAge == "adulte") {
-            niveau += 10;
-        }
-        else if (this.catAge == "vieux") {
-            niveau += 5;
-        }
-        niveau += 24-"αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(this.getRang());
-        return niveau + this.facteurDomination + force;
-    }
-
-    public String getCatAge() {
-        return catAge;
-    }
-
-    public void setCatAge(String catAge) {
-        this.catAge = catAge;
     }
 
     public int getForce() {
@@ -65,14 +121,6 @@ public class Lycanthrope {
         this.force = force;
     }
 
-    public int getFacteureDomination() {
-        return facteurDomination;
-    }
-
-    public void setFacteureDomination(int facteureDomination) {
-        this.facteurDomination = facteureDomination;
-    }
-
     public String getRang() {
         return rang;
     }
@@ -81,44 +129,70 @@ public class Lycanthrope {
         this.rang = rang;
     }
 
-    public int getNiveau() {
-        return niveau;
+    public Meute getMeute() {
+        return meute;
     }
 
-    public void setNiveau(int niveau) {
-        this.niveau = niveau;
-    }
-
-    public int getImpetuosite() {
-        return impetuosite;
-    }
-
-    public void setImpetuosite(int impetuosite) {
-        this.impetuosite = impetuosite;
-    }
-
-    public Meute getMeute(){
-        return this.meute;
-    }
-
-    public void setMeute(Meute meute){
+    public void setMeute(Meute meute) {
         this.meute = meute;
     }
 
-    public void veillir(){
-        switch (this.catAge){
-            case "jeune":
-                this.setCatAge("adulte");
+    public int getFacteurDomination() {
+        return facteurDomination;
+    }
+
+    public void setFacteurDomination(int facteurDomination) {
+        this.facteurDomination = facteurDomination;
+    }
+
+    public String getCatAge() {
+        return catAge;
+    }
+
+    public void setCatAge(String catAge) {
+        this.catAge = catAge;
+    }
+
+    /**
+     * Fait hurler le Lycanthrope en fonction du type de hurlement.
+     * Cela peut être pour marquer l'appartenance à la meute, la domination, la soumission ou l'agressivité.
+     *
+     * @param type Le type de hurlement ("appartenance", "domination", "soumission", "agressivité").
+     */
+    public void hurler(String type) {
+        switch (type) {
+            case "appartenance":
+                this.meute.hurlement(this);
+                this.toString();  // A revoir si cela fait réellement quelque chose
                 break;
-            case "adulte":
-                this.setCatAge("vieux");
+            case "domination":
+                this.toString();  // A revoir si cela fait réellement quelque chose
                 break;
-            case "vieux":
-                this.getMeute().enleverLycanthrope(this);
+            case "soumission":
+                this.toString();  // A revoir si cela fait réellement quelque chose
+                break;
+            case "agressivité":
+                this.toString();  // A revoir si cela fait réellement quelque chose
                 break;
         }
     }
 
+    /**
+     * Fait quitter le Lycanthrope de la meute.
+     * Cette méthode supprime la référence à la meute et remet à zéro les attributs liés à la domination.
+     */
+    public void quitteMeute() {
+        this.meute = null;
+        this.rang = null;
+        this.facteurDomination = 0;
+    }
+
+    /**
+     * Méthode qui retourne une représentation textuelle du Lycanthrope.
+     * Cela inclut son sexe, sa catégorie d'âge, sa force, son rang, son niveau, et la meute à laquelle il appartient.
+     *
+     * @return La représentation textuelle du Lycanthrope.
+     */
     @Override
     public String toString() {
         return "Lycantrope{" +
@@ -131,56 +205,5 @@ public class Lycanthrope {
                 ", impetuosite='" + impetuosite + '\'' +
                 ", nomMeute='" + meute.getNom() + '\'' +
                 '}';
-    }
-
-    public void domination(Lycanthrope lycanthrope){
-
-        if (this.getForce()*this.impetuosite >= lycanthrope.getForce() && !(lycanthrope.getSexe() == "femelle" && lycanthrope.getRang() == "α")){
-            if (this.getForce() > lycanthrope.getForce() || lycanthrope.getRang() == "ω"){
-                if("αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(this.rang) <= "αβγδεζηθικλμνξοπρσςτυφχψω".indexOf(lycanthrope.getRang()))
-                    Meute tmp = this.meute;
-                    this.setMeute(lycanthrope.getMeute);
-                    lycanthrope.setMeute(tmp);
-                    this.calculDomination();
-                    lycanthrope.calculDomination();
-            }
-            else {
-                this.setFacteureDomination(this.getFacteureDomination()+1);
-                lycanthrope.setFacteureDomination(lycanthrope.getFacteureDomination()-1);
-            }
-        }
-    }
-
-
-
-    public void hurler(String type){
-        switch (type){
-            case "appartenance":
-                this.meute.hurlement();
-                this.toString();
-                break;
-            case "domination":
-                this.toString();
-                break;
-            case "soumission":
-                this.toString();
-                break;
-            case "agressivité":
-                this.toString();
-                break;
-        }
-    }
-
-    public void quitteMeute(){
-        this.meute = null;
-        this.rang = null;
-        this.facteurDomination = 0;
-    }
-
-    public void transforme() {
-        if (niveau >= 100){
-            // this.   a faire
-        }
-
     }
 }
--- src/main/java/com/TP4/Meute.java
+++ src/main/java/com/TP4/Meute.java
@@ -5,70 +5,125 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Meute {
-    private String nom;
-    private String hurlement;
-    private CoupleAlpha couple;
-    private ArrayList<Lycanthrope> lycanthropes;
+    private String nom;  // Nom de la meute
+    private String hurlement;  // Hurlement de la meute
+    private CoupleAlpha couple;  // Couple Alpha de la meute
+    private ArrayList<Lycanthrope> lycanthropes;  // Liste des Lycanthropes appartenant à la meute
 
+    /**
+     * Constructeur de la classe Meute.
+     * Initialise une meute avec un nom, un hurlement et un couple Alpha.
+     *
+     * @param nom Le nom de la meute.
+     * @param hurlement Le hurlement associé à la meute.
+     * @param couple Le couple Alpha de la meute.
+     */
     public Meute(String nom, String hurlement, CoupleAlpha couple){
         this.couple = couple;
         this.nom = nom;
         this.hurlement = hurlement;
-        this.lycanthropes = new ArrayList();
+        this.lycanthropes = new ArrayList();  // Initialisation de la liste des Lycanthropes
     }
 
+    /**
+     * Récupère le nom de la meute.
+     *
+     * @return Le nom de la meute.
+     */
     public String getNom(){
         return nom;
     }
 
+    /**
+     * Récupère le couple Alpha de la meute.
+     *
+     * @return Le couple Alpha de la meute.
+     */
     public CoupleAlpha getCouple() {
         return couple;
     }
+
+    /**
+     * Modifie le couple Alpha de la meute.
+     *
+     * @param couple Le nouveau couple Alpha.
+     */
     public void setCouple(CoupleAlpha couple) {
         this.couple = couple;
     }
 
+    /**
+     * Ajoute un Lycanthrope à la meute.
+     *
+     * @param idv Le Lycanthrope à ajouter à la meute.
+     */
     public void ajouterLycanthrope(Lycanthrope idv){
         lycanthropes.add(idv);
     }
+
+    /**
+     * Enlève un Lycanthrope de la meute.
+     * Parcourt la liste des Lycanthropes pour retirer celui spécifié.
+     *
+     * @param idv Le Lycanthrope à enlever.
+     */
     public void enleverLycanthrope(Lycanthrope idv){
+        // Il est préférable d'utiliser une boucle for-each avec un break ou une approche itérative
+        // pour éviter les problèmes d'index lors de la modification de la liste pendant l'itération
         for (Lycanthrope lycanthrope : lycanthropes) {
-            lycanthropes.remove(idv);
-        }
-    }
-
-    public ArrayList<Lycanthrope> getLycanthropes() {
-        return lycanthropes;
-    }
-
-    public void reproduction(){
-        Random random = new Random();
-        int porte = random.nextInt(7) + 1;
-        String sexe = random.nextBoolean() ? "M" : "F";
-        int force = random.nextInt(15) + 1 ;
-        String rang = "γ";
-
-        Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < porte; i++) {
-            System.out.print("Lycanthrope ("+ sexe +" n°" + (i+1));
-            System.out.print("Force : " + force);
-            System.out.print("Rang : " + rang);
-            System.out.print("Nom :");
-            String nom = sc.nextLine();
-            Lycanthrope jeuneLycanthrope = new Lycanthrope(nom, sexe, "jeune", force, this);
-            lycanthropes.add(jeuneLycanthrope);
-        }
-    }
-
-    public void hurlement(Lycanthrope initial){
-        for (Lycanthrope lycanthrope : lycanthropes) {
-            if (lycanthrope != initial){
-                lycanthrope.hurler("reponse");
+            if (lycanthrope == idv) {
+                lycanthropes.remove(idv);
+                break;  // Sort de la boucle après avoir trouvé et supprimé le Lycanthrope
             }
         }
     }
 
+    /**
+     * Récupère la liste des Lycanthropes dans la meute.
+     *
+     * @return La liste des Lycanthropes.
+     */
+    public ArrayList<Lycanthrope> getLycanthropes() {
+        return lycanthropes;
+    }
 
+    /**
+     * Gère la reproduction au sein de la meute.
+     * Crée un nombre aléatoire de Lycanthropes jeunes, avec un sexe, une force et un rang aléatoires.
+     * Le nom de chaque nouveau Lycanthrope est demandé à l'utilisateur via un scanner.
+     */
+    public void reproduction(){
+        Random random = new Random();
+        int porte = random.nextInt(7) + 1;  // Nombre de Lycanthropes à créer, entre 1 et 7
+        String sexe = random.nextBoolean() ? "M" : "F";  // Sexe aléatoire
+        int force = random.nextInt(15) + 1 ;  // Force aléatoire entre 1 et 15
+        String rang = "γ";  // Rang par défaut
 
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < porte; i++) {
+            // Affichage des informations du nouveau Lycanthrope
+            System.out.print("Lycanthrope (" + sexe + " n°" + (i+1));
+            System.out.print(" Force : " + force);
+            System.out.print(" Rang : " + rang);
+            System.out.print(" Nom :");
+            String nom = sc.nextLine();  // Demande à l'utilisateur d'entrer le nom
+            Lycanthrope jeuneLycanthrope = new Lycanthrope(nom, sexe, "jeune", force, this);
+            lycanthropes.add(jeuneLycanthrope);  // Ajoute le nouveau Lycanthrope à la meute
+        }
+    }
+
+    /**
+     * Fait hurler tous les Lycanthropes de la meute en réponse à un hurlement initial.
+     * Chaque Lycanthrope hurle en réponse à l'initiateur du hurlement, à l'exception de l'initiateur.
+     *
+     * @param initial Le Lycanthrope à l'origine du hurlement.
+     */
+    public void hurlement(Lycanthrope initial){
+        for (Lycanthrope lycanthrope : lycanthropes) {
+            if (lycanthrope != initial) {
+                lycanthrope.hurler("reponse");  // Les autres Lycanthropes hurlent en réponse
+            }
+        }
+    }
 }
